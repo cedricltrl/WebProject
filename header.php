1d08da0201fd3eef018ee2ee795bb29afd87b778
Achille <achille.brossier@viacesi.fr>
2019-11-08 13:30:44 +0100

    Update 2.1
    Connexion
    Inscription
    Header

---
+++ header.php
@@ -0,0 +1,44 @@
+
+<nav class="navbar navbar-expand-md bg-dark">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="WebP/main.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Events</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Autres services
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">BDE</a>
+          <a class="dropdown-item" href="#">Clubs</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Boutique</a>
+          <a class="dropdown-item" href="#">Galerie</a>
+        </div>
+      
+    </ul>
+   <!-- <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+--> 
+<div class="nav-item-end my-2 my-lg-0">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+            <li>
+                <a class="nav-link" href="connexion.php">Log in</a>
+            </li>
+            <li>
+                <a class="nav-link" href="inscription.php">Register</a>
+            </li>
+        </div>
+    </ul>
+    </div>
+  </div>
+  
+</nav>
+
